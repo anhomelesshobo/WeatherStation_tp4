4ada3dd2503efef6d0a36c763717c2ddfedfd56b
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-11-12 16:49:29 -0500

    testing

---
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -0,0 +1,75 @@
+﻿
+using Moq;
+using OpenWeatherAPI;
+using System;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+using Xunit;
+
+namespace OpenWeatherAPITests
+{
+    public class OpenWeatherProcessorTests : IDisposable
+    {
+
+        public void Dispose()
+        {
+            
+        }
+
+       
+
+        [Fact]
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+
+            var test = OpenWeatherProcessor.Instance;
+
+            test.ApiKey = null;
+            
+            Task<OpenWeatherOneCallModel> Ow = test.GetOneCallAsync();
+
+            Assert.Throws<AggregateException>(() => Ow.Result);
+        }
+
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            var test = OpenWeatherProcessor.Instance;
+
+            test.ApiKey = null;
+
+            Task<OWCurrentWeaterModel> Ow = test.GetCurrentWeatherAsync();
+
+            Assert.Throws<AggregateException>(() => Ow.Result);
+        }
+
+        [Fact]
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            var test = OpenWeatherProcessor.Instance;
+
+            ApiHelper.ApiClient = null;
+            test.ApiKey = "ApiKey";
+
+            Task<OpenWeatherOneCallModel> Ow = test.GetOneCallAsync();
+
+            Assert.Throws<AggregateException>(() => Ow.Result);
+        }
+
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            //Arrange
+
+            var Temp = OpenWeatherProcessor.Instance;
+            // Act    
+            ApiHelper.ApiClient = null;
+            Temp.ApiKey = "ApiKey";
+            // Assert
+            Task<OWCurrentWeaterModel> Ow = Temp.GetCurrentWeatherAsync();
+
+            Assert.Throws<AggregateException>(() => Ow.Result);
+        }
+    }
+}
